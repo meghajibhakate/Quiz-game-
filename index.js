eea6e4ce7a75ee273313c13c5a994d8713b45fbc
Megha Jibhakate <106766775+meghajibhakate@users.noreply.github.com>
2022-11-19 12:51:38 +0530

    update code

--- index.js
+++ index.js
@@ -1,104 +1,95 @@
 
-function inputHide() {
-
-   document.getElementById("container").style.display = 'none'
-   document.getElementById("main-div").style.display = "flex"
-   let inputField = document.getElementById("myInput");
-   var welcomeName = document.getElementById("welcome-name")
-   welcomeName.innerHTML = "Welcome" + " " + inputField.value
-   console.log(welcomeName)
-}
-
-
-
 
 const quizDB = [
    {
-      question: "What is capital of India?",
-      a: "gandhinagar",
-      b: "Surat",
-      c: "Delhi",
-      d: "mumbai",
+      question: "https://media.istockphoto.com/id/172411313/photo/juicy-orange-refreshment.jpg?s=612x612&w=0&k=20&c=jhM16H0e3neuiyygel3DfSinTPxfbPrMcblMZUFUUjs=",
+      a: "Mango",
+      b: "Orange",
+      c: "Apple",
+      d: "Banana",
       correct: "c"
 
    },
    {
-      question: "What is the capital of Gujarat",
-      a: "surat",
-      b: "vadodara",
-      c: "gandhinagar",
-      d: "rajkot",
+      question: "https://cdn2.sharechat.com/loinwallpaper_7372653_1598360694835_cmprsd_40.jpg",
+      a: "tiger",
+      b: "bear",
+      c: "lion",
+      d: "deer",
       correct: "c"
 
    }
 ]
 
+var mainDiv=document.getElementById('main-div');
 
-const Quiz = document.getElementById('quiz')
-const answerElems = document.querySelectorAll('.answer')
-
-const questionel = document.getElementById('question')
-
-const option1 = document.getElementById('option1')
-const option2 = document.getElementById('option2')
-const option3 = document.getElementById('option3')
-const option4 = document.getElementById('option4')
+let inputField = document.getElementById("myInput");
 
 
-const submitBtn = document.getElementById('sumbit')
+function inputHide() {
 
-let score = 0;
-let currentQuiz = 0;
+   document.getElementById("container").style.display = 'none'
 
-loadQuestions()
-function loadQuestions() {
-   notSelectedAns()
+   mainDiv.innerHTML=`
 
-   const questionList = quizDB[currentQuiz]
-   //    // console.log(quiz[queCount])
-   questionel.innerText = questionList.question
 
-   //    // for the option
-   option1.innerHTML = questionList.a
-   option2.innerHTML = questionList.b
-   option3.innerHTML = questionList.c
-   option4.innerHTML = questionList.d
+   <div id="main-div"  >
+   <div class="container">
+   <h3 id="welcome-name">${"Welcome "+inputField.value}</h3>
+   <div id="box2">
+       <p id="quiz"></p>
+       <h4 id="question"></h4>
+       <img src="${quizDB[1].question}" alt="">
+       
+
+   </div>
+   <ul id="AnsOptions">
+       <li>
+           <input type="radio" name="answer" id="ans1" class="answer">
+           <label for="ans1" id="option1">${quizDB[1].a}</label>
+       </li>
+
+       <li>
+           <input type="radio" name="answer" id="ans1" class="answer">
+           <label for="ans1" id="option2">${quizDB[1].b}</label>
+       </li>
+
+       <li>
+           <input type="radio" name="answer" id="ans1" class="answer">
+           <label for="ans1" id="option3">${quizDB[1].c}</label>
+       </li>
+
+       <li>
+           <input type="radio" name="answer" id="ans1" class="answer">
+           <label for="ans1" id="option4">${quizDB[1].d}</label>
+       </li>
+
+   </ul>
+
+
+
+</div>
+
+</div>
+   `
 }
 
 
 
-function notSelectedAns() {
-   answerElems.forEach(answerElem => answerElem.checked = false)
-}
-
-function selectedAns() {
-   let answer
-   answerElems.forEach(answerElem => {
-      if(answerElem.checked){
-         answer = answerElem.id
-      }
-   })
-   console.log(answer)
-   return answer
-}
 
 
-submitBtn.addEventListener('click', () => {
-   const answer = selectedAns();
-   if (answer) {
-      if (answer === quizDB[currentQuiz].correct) {
-         score++
-      }
-      currentQuiz++
 
-      if (currentQuiz < quizDB.length) {
-         loadQuestions()
-      }
-      else {
-         Quiz.innerHTML = `
-         <h2>you answerd ${score}/${quizDB.length}question correct</h2>
-         <button onclick="location.reload()">Reload</button>
-         `
-      }
-   }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
